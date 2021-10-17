bb2016414d8ea6f2862a0a7fcb1dd94004079297
przemekbil <77756713+przemekbil@users.noreply.github.com>
2021-10-17 16:59:37 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -12,38 +12,22 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 
 # Create TabularDataset using TabularDatasetFactory
 # Data is located at:
-datastore_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+data_path = "Sinter_data.csv"
 
-ds = TabularDatasetFactory.from_delimited_files(path=datastore_path)
+ds = TabularDatasetFactory.from_delimited_files(path=data_path)
 
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+    # Clean the data
 
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+    # Remove the rows with empty values
+    x_df = data.dropna()
+    x_df = pd.get_dummies(x_df, columns=[ 'PROD_CDE','PREP ASSOCIATE','FURN'])
     
-    # remove duration column
-    x_df = x_df.drop("duration", axis=1)    
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    # Remove the WO-MRR NUM and has MRR column
+    x_df = x_df.drop("WO-MRR NUM", axis=1)
+    x_df = x_df.drop("Has MRR", axis=1)
+    
+    y_df = x_df.pop("HAS SCRAP AT SINTER")
     
     return x_df, y_df
 
