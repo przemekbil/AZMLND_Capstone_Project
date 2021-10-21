d876b4e938dc53a700afc8c0b0cf64d4df8a0100
przemekbil <77756713+przemekbil@users.noreply.github.com>
2021-10-21 22:04:49 +0000

    Add files via upload

--- score.py
+++ score.py
@@ -9,7 +9,7 @@ def init():
     global model
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.joblib')
     model = joblib.load(model_path)
 
 def run(raw_data):
